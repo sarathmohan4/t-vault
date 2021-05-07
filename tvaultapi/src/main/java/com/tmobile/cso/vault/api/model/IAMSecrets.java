1029427f658b221d38e4e2f0af945a732812fc21
alexrajan3 <alex.anantha3@t-mobile.com>
2021-05-07 16:58:01 +0530

    Onboard IAM service account changes

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/model/IAMSecrets.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/model/IAMSecrets.java
@@ -18,14 +18,9 @@
 package com.tmobile.cso.vault.api.model;
 
 import java.io.Serializable;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
+import io.swagger.annotations.ApiModelProperty;
 
 public class IAMSecrets implements Serializable {
 
@@ -34,6 +29,7 @@ public class IAMSecrets implements Serializable {
 	@Size(min = 16, max = 128, message = "AccessKeyId specified should be minimum 16 chanracters and maximum 128 characters only")
 	private String accessKeyId;
 
+	@ApiModelProperty(hidden = true)
 	private Long expiryDateEpoch;
 
 	public IAMSecrets() {
@@ -41,7 +37,6 @@ public class IAMSecrets implements Serializable {
 	}
 
 	/**
-	 *
 	 * @param accessKeyId
 	 * @param expiryDateEpoch
 	 */
